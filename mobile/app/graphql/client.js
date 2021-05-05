a638ce0047f8024346f270e1cebb69d03debc20d
Davy Vong <24511732+davyvong@users.noreply.github.com>
2021-05-05 01:13:17 -0400

    revert: apollo uri

--- mobile/app/graphql/client.js
+++ mobile/app/graphql/client.js
@@ -28,7 +28,7 @@ const authLink = setContext(async (request, context) => {
 });
 
 const httpLink = createHttpLink({
-  uri: 'http://192.168.0.24:3000/graphql',
+  uri: 'https://api.davyvong.com/budget/graphql',
 });
 
 const client = new ApolloClient({
