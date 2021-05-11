15aa0dfb950655e483f2611b2aa6ce2a519e9dde
Davy Vong <24511732+davyvong@users.noreply.github.com>
2021-05-10 20:08:43 -0400

    feat: window dimensions

--- mobile/app/hooks/dimensions.js
+++
@@ -1,6 +0,0 @@
-import DimensionsContext from 'contexts/dimensions';
-import { useContext } from 'react';
-
-const useDimensions = () => useContext(DimensionsContext);
-
-export default useDimensions;
