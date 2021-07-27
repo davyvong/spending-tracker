32ef7262b025db84b1430e98092941a492c2065a
Davy Vong <24511732+davyvong@users.noreply.github.com>
2021-07-27 11:28:46 -0400

    feat: btc and eth as supported currencies

--- mobile/app/constants/currencies.js
+++ mobile/app/constants/currencies.js
@@ -45,7 +45,7 @@ const currencyMap = {
 
 export const getCurrency = currencyCode => currencyMap[currencyCode];
 
-export const getSupportedCurrencies = () => ['AUD', 'CAD', 'CHF', 'CNY', 'EUR', 'GBP', 'JPY', 'USD'];
+export const getSupportedCurrencies = () => ['AUD', 'BTC', 'CAD', 'CHF', 'CNY', 'ETH', 'EUR', 'GBP', 'JPY', 'USD'];
 
 export const currencyOptions = getSupportedCurrencies().map(currencyCode => {
   const currency = getCurrency(currencyCode);
