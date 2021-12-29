cb622eaf4f4d84ab8843c270d16c544f4338b484
Davy Vong <24511732+davyvong@users.noreply.github.com>
2021-12-29 02:31:57 -0500

    feat: add barcode to root

--- api/src/data-sources/root-data-sources.js
+++ api/src/data-sources/root-data-sources.js
@@ -1,15 +1,18 @@
 import VendorDataSource from 'data-sources/api/vendor';
 import AccountDataSource from 'data-sources/mongo/account';
+import BarcodeDataSource from 'data-sources/mongo/barcode';
 import CardDataSource from 'data-sources/mongo/card';
 import CategoryDataSource from 'data-sources/mongo/category';
 import TransactionDataSource from 'data-sources/mongo/transaction';
 import AccountSchema from 'mongoose/schemas/account';
+import BarcodeSchema from 'mongoose/schemas/barcoode';
 import CardSchema from 'mongoose/schemas/card';
 import CategorySchema from 'mongoose/schemas/category';
 import TransactionSchema from 'mongoose/schemas/transaction';
 
 export default mongoose => ({
   account: new AccountDataSource(mongoose.model('Account', AccountSchema)),
+  barcode: new BarcodeDataSource(mongoose.model('Barcode', BarcodeSchema)),
   card: new CardDataSource(mongoose.model('Card', CardSchema)),
   category: new CategoryDataSource(mongoose.model('Category', CategorySchema)),
   transaction: new TransactionDataSource(mongoose.model('Transaction', TransactionSchema)),
