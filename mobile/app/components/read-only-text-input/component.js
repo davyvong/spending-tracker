b63edcdccd9ab2b6bd6434c490e3b27143da05a1
Davy Vong <24511732+davyvong@users.noreply.github.com>
2021-12-29 20:45:38 -0500

    fix: copy to clipboard for ReadOnlyTextInput component

--- mobile/app/components/read-only-text-input/component.js
+++ mobile/app/components/read-only-text-input/component.js
@@ -1,26 +1,22 @@
 import { MaterialIcons } from '@expo/vector-icons';
 import PropTypes from 'prop-types';
 import React, { Fragment } from 'react';
-import { ActivityIndicator, Pressable, TextInput } from 'react-native';
+import { Pressable, TextInput } from 'react-native';
 
 import styles from './styles';
 
-const ReadOnlyTextInputComponent = ({ copyToClipboard, pending, theme, ...props }) => (
+const ReadOnlyTextInputComponent = ({ copyToClipboard, isCopied, theme, ...props }) => (
   <Fragment>
     <TextInput {...props} editable={false} onTextChange={() => {}} pointerEvents="none" />
-    <Pressable onPress={copyToClipboard} style={styles.copyIcon}>
-      {pending ? (
-        <ActivityIndicator color={theme.defaultIcon} />
-      ) : (
-        <MaterialIcons color={theme.defaultIcon} name="content-copy" size={20} />
-      )}
+    <Pressable disabled={isCopied} onPress={copyToClipboard} style={styles.copyIcon}>
+      <MaterialIcons color={theme.defaultIcon} name={isCopied ? 'check' : 'content-copy'} size={20} />
     </Pressable>
   </Fragment>
 );
 
 ReadOnlyTextInputComponent.propTypes = {
   copyToClipboard: PropTypes.func.isRequired,
-  pending: PropTypes.bool.isRequired,
+  isCopied: PropTypes.bool.isRequired,
   theme: PropTypes.object.isRequired,
 };
 
