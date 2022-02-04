ccdedc22ab434565a13bb3425402d2119efe956d
Davy Vong <24511732+davyvong@users.noreply.github.com>
2022-02-04 11:32:48 -0500

    fix: barcode labels and icon positions

--- mobile/app/components/barcode-scanner-input/styles.js
+++ mobile/app/components/barcode-scanner-input/styles.js
@@ -2,9 +2,9 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   scannerIcon: {
-    bottom: 10,
     position: 'absolute',
     right: 10,
+    top: 36,
   },
   modalContent: {
     flex: 1,
